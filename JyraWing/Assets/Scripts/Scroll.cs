628ebe6c40d3201d0314d5d8d66bbdad36dd61b4
rCori <ryancori1@gmail.com>
2015-06-25 23:41:37 -0700

    Fixed infinite scrolling
    Repositioning after an infinite background piece falls offsecreen is now
    greatly improved. The math before was incorrect and relied on the size
    of the window itself being consistent and the background being a
    particular size. Now as long as a background covers the entire camera
    view screen or more, it should just work.

--- JyraWing/Assets/Scripts/Scroll.cs
+++ JyraWing/Assets/Scripts/Scroll.cs
@@ -5,13 +5,24 @@ public class Scroll : MonoBehaviour {
 
 	public float speed;
 	public bool infinite;
-	private float pixelsToUnits;
-	private float width;
+	private float spriteWidth;
+	//If the background is wider than the camera's viewport and it needs to infinite scroll
+	//We must move the background farther than just the edge of the camera, but farther to compensate.
+	//This should show all of the background each time.
+	private float extraWidth;
+	private float horzExtent;
 
 	// Use this for initialization
 	void Start(){
-		pixelsToUnits = 0.01f;
-		width = GetComponent<Renderer>().bounds.size.x;
+		float vertExtent = Camera.main.orthographicSize*2;
+		//horzontal extent is how many game units across the camera sees.
+		horzExtent = vertExtent * Screen.width / Screen.height;
+		spriteWidth = GetComponent<Renderer>().bounds.size.x;
+		extraWidth = spriteWidth - horzExtent;
+		if (extraWidth < 0 && infinite) {
+			Debug.Log ("Scrolling background is not wide enough");
+			extraWidth = 0;
+		}
 	}
 	
 	// Update is called once per frame
@@ -21,7 +32,8 @@ public class Scroll : MonoBehaviour {
 
 	void OnBecameInvisible(){
 		if (infinite) {
-			transform.position = new Vector2((pixelsToUnits *Screen.width) + (width/2), 0);
+			Vector3 newPos = new Vector3(horzExtent + (extraWidth*3/2), 0f, 0f);
+			transform.position = newPos;
 		}
 	}
 }
