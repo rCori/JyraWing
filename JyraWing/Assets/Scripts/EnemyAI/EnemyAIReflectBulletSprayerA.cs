f26abb1db3ae9c177b1b5a71c612b0bbb5886224
RyanCori <ryancori1@gmail.com>
2016-04-19 23:37:20 -0700

    New enemy reflectorBulletSprayer
    New enemy made not implemented in any level yet

---
+++ JyraWing/Assets/Scripts/EnemyAI/EnemyAIReflectBulletSprayerA.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyAIReflectBulletSprayerA : EnemyBehavior
+{
+    public List<Vector2> locations;
+    public List<float> times;
+
+    public float fireRate = 2.0f;
+    private float fireTimer;
+    private int currentMovementStep;
+
+    public float bulletSpeed = 0.5f;
+
+    private Vector2[] fireDirections;
+
+	void Awake() {
+        EnemyDefaults();
+        AudioClip explosionClip = Resources.Load("Audio/SFX/explosion2") as AudioClip;
+        SetExplosionSfx(explosionClip);
+
+        HasAnimations animationsOwned;
+        animationsOwned = HasAnimations.Hit | HasAnimations.Destroy;
+
+        SetAnimations(animationsOwned);
+        SetHitAnimationName("reflectBulletSprayer_hit");
+
+        currentMovementStep = 0;
+        fireTimer = 0.0f;
+
+        fireDirections = new Vector2[8];
+        fireDirections[0] = new Vector2(1.0f, 0.0f).normalized * bulletSpeed;
+        fireDirections[1] = new Vector2(0.5f, 0.5f).normalized * bulletSpeed;
+        fireDirections[2] = new Vector2(0.0f, 1.0f).normalized * bulletSpeed;
+        fireDirections[3] = new Vector2(-0.5f, 0.5f).normalized * bulletSpeed;
+        fireDirections[4] = new Vector2(-1.0f, 0.0f).normalized * bulletSpeed;
+        fireDirections[5] = new Vector2(-0.5f, -0.5f).normalized * bulletSpeed;
+        fireDirections[6] = new Vector2(0.0f, -1.0f).normalized * bulletSpeed;
+        fireDirections[7] = new Vector2(0.5f, -0.5f).normalized * bulletSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_paused)
+        {
+            return;
+        }
+        Movement();
+        if (GetIsTimeUp()){
+            if (currentMovementStep < locations.Count){
+                StartNewMovement(locations[currentMovementStep], times[currentMovementStep]);
+                currentMovementStep++;
+            } else {
+                Destroy(gameObject);
+            }
+        }
+
+
+
+        fireTimer += Time.deltaTime;
+        if(fireTimer > fireRate){
+            Shoot(fireDirections[0], true);
+            Shoot(fireDirections[1], true);
+            Shoot(fireDirections[2], true);
+            Shoot(fireDirections[3], true);
+            Shoot(fireDirections[4], true);
+            Shoot(fireDirections[5], true);
+            Shoot(fireDirections[6], true);
+            Shoot(fireDirections[7], true);
+            fireTimer = 0.0f;
+        }
+
+        HandleHitAnimation();
+    }
+}
---
+++ JyraWing/Assets/Scripts/EnemySpawns/EnemySpawnReflectBulletSprayer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemySpawnReflectBulletSprayer : EnemySpawner {
+
+	public Vector2 enemyPosition;
+
+	public int Health;
+
+    public List<Vector2> locations;
+    public List<float> times;
+
+    public float fireRate;
+    public float bulletSpeed;
+
+    public override void Spawn ()
+	{
+		EnemyBulletPool bulletPool = GameObject.Find ("EnemyBulletPool").GetComponent<EnemyBulletPool> ();
+		EnemyBulletPool shieldableBulletPool = GameObject.Find ("EnemyShieldableBulletPool").GetComponent<EnemyBulletPool> ();
+		
+		GameObject enemy = (GameObject) Resources.Load ("Enemies/Enemy_ReflectBulletSprayer");
+		enemy.transform.position = enemyPosition;
+		
+		enemy.GetComponent<EnemyBehavior> ().bulletPool= bulletPool;
+		enemy.GetComponent<EnemyBehavior> ().shieldableBulletPool= shieldableBulletPool;
+		enemy.GetComponent<EnemyBehavior> ().LeftWallException = false;
+
+		enemy = Instantiate (enemy);
+
+        enemy.GetComponent<EnemyBehavior>().SetEnemyHealth(Health);
+        enemy.GetComponent<EnemyAIReflectBulletSprayerA>().locations = locations;
+        enemy.GetComponent<EnemyAIReflectBulletSprayerA>().times = times;
+
+        enemy.GetComponent<EnemyAIReflectBulletSprayerA>().fireRate = fireRate;
+        enemy.GetComponent<EnemyAIReflectBulletSprayerA>().bulletSpeed = bulletSpeed;
+
+    }
+}
