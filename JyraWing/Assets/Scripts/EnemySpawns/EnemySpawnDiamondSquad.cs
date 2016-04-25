389b2a5f95784a5479e0057543dfb5cd78f82bf7
RyanCori <ryancori1@gmail.com>
2016-04-24 23:47:20 -0700

    First draft level 3
    A full but very rough level 3 with no boss is done

---
+++ JyraWing/Assets/Scripts/EnemySpawns/EnemySpawnDiamondSquad.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawnDiamondSquad : EnemySpawner {
+
+	public Vector2 enemyPosition;
+	public Vector2 enemyDirection;
+	public float enemyTime;
+	public bool enemyRepeat;
+	public int enemyHitPoints;
+
+	public int rows;
+	public int columns;
+
+	public float rowSpacing;
+	public float columnSpacing;
+	public float yShift;
+
+	public override void Spawn () {
+
+		float yOffset = enemyPosition.y -columns / 2f + yShift;
+		float xOffset = enemyPosition.x;
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				GameObject enemy1 = (GameObject)Resources.Load ("Enemies/Enemy_H");
+				enemy1.transform.position = new Vector2 (xOffset + i*rowSpacing, yOffset + j*columnSpacing);
+
+				EnemyBehavior enemyBehavior1 = enemy1.GetComponent<EnemyBehavior> ();
+				if (enemyRepeat) {
+					enemyBehavior1.LeftWallException = false;
+				}
+
+				EnemyAI8 enemyAI1 = enemy1.GetComponent<EnemyAI8> ();
+				enemyAI1.direction = enemyDirection;
+				enemyAI1.time = enemyTime + i*enemyDirection.magnitude;
+				enemyAI1.repeat = enemyRepeat;
+
+				enemy1 = Instantiate (enemy1);
+				enemy1.GetComponent<EnemyBehavior> ().SetEnemyHealth (enemyHitPoints);
+			}
+		}
+	}
+}
---
+++ JyraWing/Assets/Scripts/EnemySpawns/EnemySpawnShipSquad.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawnShipSquad : EnemySpawner {
+
+	public int rows;
+	public int columns;
+
+	public float rowSpacing;
+	public float columnSpacing;
+	public float yShift;
+
+	public int shipHealth;
+	public float speed;
+	public float lifeTime;
+	public float fireRate;
+	public float bulletSpeed;
+	public bool shieldableBullets;
+
+	public override void Spawn ()
+	{
+
+		float yOffset = -columns / 2f + yShift;
+		float xOffset = 8.0f;
+
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				EnemyBulletPool bulletPool = GameObject.Find ("EnemyBulletPool").GetComponent<EnemyBulletPool> ();
+				EnemyBulletPool shieldableBulletPool = GameObject.Find ("EnemyShieldableBulletPool").GetComponent<EnemyBulletPool> ();
+
+				//Middle row
+				GameObject enemy = (GameObject)Resources.Load ("Enemies/Enemy_F");
+				enemy.transform.position = new Vector2 (xOffset + i*rowSpacing, yOffset + j*columnSpacing);
+
+				enemy.GetComponent<EnemyBehavior> ().bulletPool = bulletPool;
+				enemy.GetComponent<EnemyBehavior> ().shieldableBulletPool = shieldableBulletPool;
+				enemy.GetComponent<EnemyBehavior> ().LeftWallException = true;
+				enemy.GetComponent<EnemyBehavior> ().shieldableBullets = false;
+				EnemyAI6 ai1 = enemy.GetComponent<EnemyAI6> ();
+				ai1.angle = 180;
+				ai1.speed = speed;
+				ai1.lifeTime = lifeTime + i*speed;
+				ai1.fireRate = fireRate;
+				ai1.bulletSpeed = bulletSpeed;
+				ai1.hits = shipHealth;
+				ai1.shootInDirection = true;
+				enemy.GetComponent<Scroll> ().speed = 1;
+				enemy = Instantiate (enemy);
+			}
+		}
+	}
+}
